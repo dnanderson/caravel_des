52d581b9360d169603b991db061ea205fe5fc5c8
Derek Anderson <dereknathananderson@gmail.com>
2021-11-29 22:06:39 -0700

    add source script, modify it to suite your filesystem

---
+++ sourcme.sh
@@ -0,0 +1,12 @@
+# You need to modify each of these paths to be absolute paths
+# on your own filesystem.
+
+# This is where the PDK should go, modify this path
+export PDK_ROOT=/home/derek/mpw_proj/skywater-pdk
+# This is where the OpenLane project git project should go
+export OPENLANE_ROOT=/home/derek/mpw_proj/OpenLane
+# This is inside of the main project
+export CARAVEL_ROOT=/home/derek/mpw_proj/caravel_des/caravel
+# This is the root of our git repository
+export INPUT_DIRECTORY=/home/derek/mpw_proj/caravel_des
+
